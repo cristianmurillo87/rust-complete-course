cf8130e840610274fce3154cc8b494d8c489dc47
Cristian  Murillo <cam_725@hotmail.com>
2025-03-29 22:33:16 +0100

    documented use of the result enum with matchin and the try operator

--- 07-handling-result-enum/src/main.rs
+++ 07-handling-result-enum/src/main.rs
@@ -10,7 +10,15 @@ fn validate_email(email: String) -> Result<(), Error> {
 }
 
 fn read_file(path: &str) -> Result<String, Error> {
-    fs::read_to_string(path)
+    // the try operator is used (?) can only be used after functions that return a 'Result' enum,
+    // if the result is Ok(), it returns the value contained within it,
+    // otherwise it returns the Error variant
+    let result = fs::read_to_string(path)?;
+    Ok(result)
+}
+
+fn write_file(path: &str, content: String) -> Result<(), Error> {
+    fs::write(path, content)
 }
 
 fn extract_log_errors(errors: &str) -> Vec<&str> {
@@ -21,30 +29,21 @@ fn extract_log_errors(errors: &str) -> Vec<&str> {
             .collect()
 }
 
-fn main() {
-    // path to file relative to the project root
-    let text = fs::read_to_string("logs.txt");
-
-    match text {
-        Ok(result) =>  println!("{:#?}", result),
-        Err(explanation) => println!("An error occurred while trying to read the file!!\n{:#?}", explanation)
-    }
-
+fn main(){
     match validate_email(String::from("something@provider.com")) {
         // _ indicates the value inside the result is not relevant for the evaluation
         Ok(_) => println!("Email is valid"),
         Err(_) => println!("Invalid email!")
     }
 
+     // path to file relative to the project root
     match read_file("logs.txt") {
         Ok(result) => {
             let error_lines = extract_log_errors(&result);
             if error_lines.len() == 0 {
                 println!("No errors found in log file");
             } else {
-                for line in error_lines {
-                    println!("{}", line);
-                }
+                write_file("errors.txt", error_lines.join("\n")).expect("Saving errors to errors.txt failed");
             }
         },
         Err(explanation) => println!("An error occurred while trying to read the file!!\n{:#?}", explanation)
